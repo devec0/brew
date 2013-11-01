6526b26f48083de65536233d74c754428f6bda69
James Hebden <james@hebden.net.au>
2013-11-02 07:40:09 +1100

    Added additional sensor for external temperature, improved heating/cooling logic

--- Brew.ino
+++ Brew.ino
@@ -3,6 +3,7 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <Ethernet.h>
+#include <string.h>
 
 //define out buttons. change these to suit your setup.
 //which analogue input have you connected your button array to?
@@ -27,17 +28,20 @@ OneWire onewire(tempPin);
 //will implement this soon.
 const int maxReadings = 10;
 int numReadings = 0;
-float tempReadings[maxReadings];   
+float internaltempReadings[maxReadings];   
+float externaltempReadings[maxReadings];   
 int readingIndex = 0;                  
 float readingTotal = 0;                 
 float readingAverage = 0;                
 
-//used when reading from the 1-wire temperature sensor. stores the address of the first DS18B20 sensor found on the bus.
-byte tempAddr[8];
+//used when reading from the 1-wire temperature sensor. stores the address of the first two DS18B20 sensors found on the bus.
+byte internaltempAddr[8];
+byte externaltempAddr[8];
 
 //some sensible starting values. plan to use the SD card to save/restore these.
 float targetTemp = 20.5;
-float currentTemp;
+float currentinternalTemp;
+float currentexternalTemp;
 float tempDiff = 2.5;
 
 //did we find a sensor? used during detection logic and loop to 
@@ -84,30 +88,76 @@ int buttonPressed = 0;
 // see the LiquidCrystal documentation for a more detailed explanationeeeeeeeeee
 LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
 
-void getCurrentTemp()
+void findTempSensors()
+{
+  int sensorCount = 0;
+  int addrCount = 0;
+  byte tempAddr[8];
+  
+  Serial.println("Searching for temperature sensor");
+  while (onewire.search(tempAddr)) {
+     Serial.println("Found one-wire device:");
+     for (addrCount = 0; addrCount < 8; addrCount++) {
+       Serial.print(tempAddr[addrCount], HEX);
+     }
+     Serial.println();
+     if (tempAddr[0] == 0x28) {
+       Serial.println("Found DS18B20!");
+       sensorCount++;
+       if (sensorCount > 1) {
+         memcpy(externaltempAddr,tempAddr,8);
+         Serial.print("Found external sensor: ");
+         for (addrCount = 0; addrCount < 8; addrCount++) {
+           Serial.print(externaltempAddr[addrCount], HEX);
+         }
+         Serial.println();
+         Serial.println("We've found both sensors.");
+         sensorFound = true;
+       } else if (sensorCount > 0) {
+         memcpy(internaltempAddr,tempAddr,8);
+         Serial.print("Found internal sensor: ");
+         for (addrCount = 0; addrCount < 8; addrCount++) {
+           Serial.print(internaltempAddr[addrCount], HEX);
+         }
+         Serial.println();
+       }
+     } else {
+       Serial.println("Device was not a DS18B20.");
+       Serial.print(tempAddr[addrCount],HEX);
+     }
+   }
+}
+
+void getCurrentTemps()
 {
   int tempByte;
   int tempData[12];
   int16_t tempRaw;
   
-  if (sensorFound && (sensorConverting < 10) && (sensorConverting > 0)) {
-    Serial.println("Incrementing sensor wait counter.");
+  if (sensorFound && (sensorConverting < 40) && (sensorConverting > 0)) {
+    //Serial.println("Incrementing sensor wait counter.");
     sensorConverting++;
   } else if (sensorFound && (sensorConverting == 0)) {
-      Serial.println("Sending a conversion request to sensor.");
+      Serial.println("Sending a conversion request to sensors.");
       //kick off a temp conversion
       onewire.reset();
-      onewire.select(tempAddr);
+      onewire.select(externaltempAddr);
+      onewire.write(0x44);
+      onewire.reset();
+      onewire.select(internaltempAddr);
       onewire.write(0x44);
       //reset quasi-timer
       sensorConverting = 1;
   } else if (sensorFound && (sensorConverting > 9)) {
-      Serial.println("Reading sensor.");
+      //lets read internal then external sensors
+      Serial.println("Reading sensors.");
       //we've hit one second (10 100 millisecond delays)
       sensorConverting = 0;
-      //lets read the value
+      //reset the bus
       onewire.reset();
-      onewire.select(tempAddr);
+      
+      //read the external (first) sensor
+      onewire.select(externaltempAddr);
       onewire.write(0xBE);
       //read all 9 bytes
       for (tempByte = 0; tempByte < 9; tempByte++) {
@@ -117,27 +167,47 @@ void getCurrentTemp()
       tempRaw = (tempData[1] << 8) | tempData[0];
       //read at correct resolution, based on config byte
       if ((tempData[4] & 0x60) == 0x00) {
-        Serial.println("Reading 9bit value.");
         tempRaw = tempRaw & ~7;
       } else if ((tempData[4] & 0x60) == 0x20) {
-        Serial.println("Reading 10bit value.");
         tempRaw = tempRaw & ~3;
       } else if ((tempData[4] & 0x60) == 0x40) {
-        Serial.println("Reading 11bit value.");
         tempRaw = tempRaw & ~1;
-      } else {
-        Serial.println("Reading 12bit value.");
-      }
-      Serial.println("Read value.");
+      } 
+      Serial.print("Read external value: ");
       Serial.print(tempRaw / 16);
-      currentTemp = (float)tempRaw / 16;
+      Serial.println();
+      currentexternalTemp = (float)tempRaw / 16;
+      
+      //lets read external value
+      onewire.reset();
+      onewire.select(internaltempAddr);
+      onewire.write(0xBE);
+      //read all 9 bytes
+      for (tempByte = 0; tempByte < 9; tempByte++) {
+        tempData[tempByte] = onewire.read();
+      }
+      //convert to something useful!
+      tempRaw = (tempData[1] << 8) | tempData[0];
+      //read at correct resolution, based on config byte
+      if ((tempData[4] & 0x60) == 0x00) {
+        tempRaw = tempRaw & ~7;
+      } else if ((tempData[4] & 0x60) == 0x20) {
+        tempRaw = tempRaw & ~3;
+      } else if ((tempData[4] & 0x60) == 0x40) {
+        tempRaw = tempRaw & ~1;
+      }
+      Serial.print("Read internal value: ");
+      Serial.print(tempRaw / 16);
+      Serial.println();
+      currentinternalTemp = (float)tempRaw / 16;
+      
   } else { 
-    Serial.println("Sensor not found?");
+    Serial.println("Sensor not found? Searching again...");
+    findTempSensors();
   }
 }
 
 void setup() {
-  int addrCount = 0;
   
   //set up serial for debug
   Serial.begin(9600);
@@ -149,14 +219,16 @@ void setup() {
   // set up the LCD's number of columns and rows:
   Serial.println("Setting up LCD"); 
   lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("TT: ");
-  lcd.setCursor(8,0);
-  lcd.print("CT: ");
+  lcd.setCursor(2,0);
+  lcd.print("I:");
+  lcd.setCursor(9,0);
+  lcd.print("E:");
   lcd.setCursor(0,1);
-  lcd.print("Init... ");
-  lcd.setCursor(8,1);
-  lcd.print("TD: ");
+  lcd.print("[I]");
+  lcd.setCursor(3,1);
+  lcd.print("T:");
+  lcd.setCursor(10,1);
+  lcd.print("D:");
   
   //turn both relays off, set as digital outputs
   Serial.println("Turning relays off");
@@ -166,63 +238,85 @@ void setup() {
   digitalWrite(coolPin, LOW);
   
   //reset readings array
-   for (int thisReading = 0; thisReading < maxReadings; thisReading++)
-    tempReadings[thisReading] = 0;  
+   for (int thisReading = 0; thisReading < maxReadings; thisReading++) {
+    internaltempReadings[thisReading] = 0;  
+    externaltempReadings[thisReading] = 0;  
+   }
     
    //search for onewire temp sensor
    delay(5000);
-   Serial.println("Searching for temperature sensor");
-   while (onewire.search(tempAddr)) {
-     Serial.println("Found one-wire device:");
-     for (addrCount = 0; addrCount < 8; addrCount++) {
-       Serial.print(tempAddr[addrCount], HEX);
-     }
-     Serial.println();
-     if (tempAddr[0] == 0x28) {
-       Serial.println("Found DS18B20!");
-       sensorFound = true;
-     } else {
-       Serial.println("Device was not a DS18B20.");
-       Serial.print(tempAddr[0],HEX);
-     }
-   }
+   findTempSensors();
 }
 
 void switchRelays() {
   //if cooling and temp less than minimum, switch to idle, turn off relays
-  if (mode == MODE_COOL && currentTemp < (targetTemp - tempDiff)) {
+  if (mode == MODE_COOL && currentinternalTemp < (targetTemp - tempDiff)) {
+    Serial.println("Idling because we've been cooling and we've reached target temperature.");
     digitalWrite(coolPin, LOW);
     digitalWrite(heatPin, LOW);
     mode = MODE_IDLE;
   //if heating and temp higher than maximum, switch to idle, turn off relays
-  } else if (mode == MODE_HEAT && currentTemp > (targetTemp + tempDiff)) {
+  } else if (mode == MODE_HEAT && currentinternalTemp > (targetTemp + tempDiff)) {
+    Serial.println("Idling because we've been heating and we've reached target temperature.");
     digitalWrite(coolPin, LOW);
     digitalWrite(heatPin, LOW);
     mode = MODE_IDLE;
-  //if idle and temp higher than maxium, start cooling
-  } else if (mode == MODE_IDLE && currentTemp > (targetTemp + tempDiff)) {
+  //if idle and temp inside is higher than outside, and temperature has not reached minimum, keep idling
+  } else if (mode == MODE_IDLE && currentinternalTemp > (targetTemp - tempDiff) && currentexternalTemp < currentinternalTemp) {
+    Serial.println("Keep idling because it's cooler outside and we're above minimum temp.");
+    digitalWrite(coolPin, LOW);
+    digitalWrite(heatPin, LOW);
+    mode = MODE_IDLE;
+  //if idle and temp inside is lower than outside, and temperature has not reached maximum, keep idling
+  } else if (mode == MODE_IDLE && currentinternalTemp < (targetTemp + tempDiff) && currentexternalTemp > currentinternalTemp) {
+    Serial.println("Keep idling because it's hotter outside and we're below maximum temp.");
+    digitalWrite(coolPin, LOW);
+    digitalWrite(heatPin, LOW);
+    mode = MODE_IDLE;
+  //if idle and temp inside is lower than temp outside, and temperature has reached maximum, time to cool
+  } else if (mode == MODE_IDLE && currentinternalTemp > (targetTemp + tempDiff) && currentexternalTemp > (targetTemp + tempDiff)) {
+    Serial.println("Start cooling because it's hotter outside and we're above maximum temp.");
     digitalWrite(coolPin, HIGH);
     digitalWrite(heatPin, LOW);
     mode = MODE_COOL;
-  //if idle and temp lower than minimum, start heating
-  } else if (mode == MODE_IDLE && currentTemp < (targetTemp + tempDiff)) {
+  //if idle and temp inside is higher than temp outsid, and temperature has reached minimum, time to heat
+  } else if (mode == MODE_IDLE && currentinternalTemp < (targetTemp - tempDiff) && currentexternalTemp < (targetTemp - tempDiff)) {
+    Serial.println("Start heating because it's colder outside and we're below minimum temp.");
     digitalWrite(coolPin, LOW);
     digitalWrite(heatPin, HIGH);
     mode = MODE_HEAT;
+  } else if (mode == MODE_COOL && currentinternalTemp >= (targetTemp - tempDiff)) {
+    Serial.println("Keep cooling, we haven't reached our goal.");
+    digitalWrite(coolPin, HIGH);
+    digitalWrite(heatPin, LOW);
+    mode = MODE_COOL;
+  } else if (mode == MODE_HEAT && currentinternalTemp <= (targetTemp + tempDiff)) {
+    Serial.println("Keep heating, we haven't reached our goal.");
+    digitalWrite(coolPin, LOW);
+    digitalWrite(heatPin, HIGH);
+    mode = MODE_HEAT;
+  } else {
+    //default position is to idle
+    Serial.println("No matched condition, keep idling.");
+    digitalWrite(coolPin, LOW);
+    digitalWrite(heatPin, LOW);
+    mode = MODE_IDLE;
   }
 }
 
 void updateLCD() {
+    //show current temp
+    lcd.setCursor(4,0);
+    lcd.print(currentinternalTemp, 1);
+    lcd.setCursor(11,0);
+    lcd.print(currentexternalTemp, 1);
+    //Serial.write(currentTemp);
     //show target temp
-    lcd.setCursor(3,0);
+    lcd.setCursor(5,1);
     lcd.print(targetTemp, 1);
     //Serial.write(targetTemp);
-    //show current temp
-    lcd.setCursor(11,0);
-    lcd.print(currentTemp, 1);
-    //Serial.write(currentTemp);
     //show temp diff
-    lcd.setCursor(11,1);
+    lcd.setCursor(12,1);
     lcd.print(tempDiff, 1);
     //Serial.write(tempDiff);
    
@@ -230,13 +324,13 @@ void updateLCD() {
     lcd.setCursor(0,1);
     switch (mode)  {
       case MODE_IDLE :
-        lcd.print("Idle    ");
+        lcd.print("[I]");
         break;
       case MODE_HEAT :
-        lcd.print("Heating ");
+        lcd.print("H>>");
         break;
       case MODE_COOL :
-        lcd.print("Cooling ");
+        lcd.print("<<C");
         break;
     }
 }
@@ -263,8 +357,12 @@ void checkClients() {
             client.println();
             client.println("{");
             // output the current temp
-            client.print("currenttemp: ");
-            client.print(currentTemp);
+            client.print("currentinternaltemp: ");
+            client.print(currentinternalTemp);
+            client.println(",");
+            // output the current external temp
+            client.print("currentexternaltemp: ");
+            client.print(currentexternalTemp);
             client.println(",");
             // output the target temp
             client.print("targettemp: ");
@@ -302,7 +400,7 @@ void loop() {
 
     //get temperature reading for primary temp
     //Serial.println("Getting temperature");
-    getCurrentTemp();
+    getCurrentTemps();
 
     //handle keypad
     int button = getButton();
