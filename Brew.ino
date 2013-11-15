1e29a3b9b7da91c98b23315f9ba9c4edffcd5159
James Hebden <james@hebden.net.au>
2013-11-15 23:08:50 +1100

    Added a whole stack of timers and extra conditionals around switching in and out of cooling mode. Also added an overshoot constant into the calculation for exiting cooling mode.

--- Brew.ino
+++ Brew.ino
@@ -2,16 +2,20 @@
 #include <SPI.h>
 #include <LiquidCrystal.h>
 #include <OneWire.h>
-#include <Ethernet.h>
+//#include <Ethernet.h>
 #include <string.h>
+#include <EEPROM.h>
+#include <enc28j60.h>
+#include <EtherCard.h>
+#include <net.h>
 
 //define out buttons. change these to suit your setup.
 //which analogue input have you connected your button array to?
-int buttonPin = A0;
+int buttonPin = A1;
 //and where is your temperature sensor connected via 1-wire?
-int tempPin = A1;
+int tempPin = A2;
 //the below is the pin that controls your heating element via a relay
-int heatPin = A5;
+int heatPin = A3;
 //the below is the pin that controls your cooling element (or fridge compressor) via a relay
 int coolPin = A4;
 
@@ -21,48 +25,47 @@ int coolPin = A4;
 //2 = heating
 int mode = 0;
 
-//initialise the OneWire library on your temperature pin
-OneWire onewire(tempPin);
+//overshoot percentages
+#define COLD_OVERSHOOT 0.1
 
-//all used for smoothing readings as they come in, to prevent spikes. 
-//will implement this soon.
-const int maxReadings = 10;
-int numReadings = 0;
-float internaltempReadings[maxReadings];   
-float externaltempReadings[maxReadings];   
-int readingIndex = 0;                  
-float readingTotal = 0;                 
-float readingAverage = 0;                
+//tracks time spent cooling
+unsigned long timeCooling = 0;
+unsigned long lastCool = 0;
+unsigned long lastIdle = 0;
+unsigned long lastHeat = 0;
+
+//initialise the OneWire library on your temperature pin
+OneWire onewire(tempPin);               
 
 //used when reading from the 1-wire temperature sensor. stores the address of the first two DS18B20 sensors found on the bus.
 byte internaltempAddr[8];
 byte externaltempAddr[8];
 
 //some sensible starting values. plan to use the SD card to save/restore these.
-float targetTemp = 20.5;
+float targetTemp = 21.0;
 float currentinternalTemp;
 float currentexternalTemp;
-float tempDiff = 2.5;
+float tempDiff = 1.0;
 
 //did we find a sensor? used during detection logic and loop to 
 //prevent us from making any temperature based decisions if the 
 //sensor was not detected.
-boolean sensorFound = false;
+boolean sensorsFound = false;
 //are we mid-conversion? needed because we only update temperature
 //once per second, however i use a 100 msec delay in the loop to keep the LCD
 //and keypad responsive
 int sensorConverting = false;
 
 //change the below if you would like a less stupid MAC on your ethernet connection
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+//byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 //the ip you would like to reach the controller on
-byte ip[] = {192,168,1,6};
+//byte ip[] = {192,168,1,6};
 //your network's internet gateway
-byte gateway[] = {192,168,1,1};
+//byte gateway[] = {192,168,1,1};
 //your network's subnet
-byte subnet[] = {255,255,255,0};
+//byte subnet[] = {255,255,255,0};
 //set up a server on port 80
-EthernetServer server(80);
+//EthernetServer server(80);
 
 //this is set when we've correctly identified that a button is being pressed,
 //so we can fire the button handler. this is reset once the button is handled and released
@@ -83,10 +86,64 @@ int buttonPressed = 0;
 #define MODE_COOL                 1
 #define MODE_HEAT                 2
 
-
+#define REST_COMPRESSOR 1780000
+#define STARTUP_DELAY 30000
+#define COMPRESSOR_MIN_RUN 30000
 // initialize the library with the numbers of the interface pins you are using
-// see the LiquidCrystal documentation for a more detailed explanationeeeeeeeeee
-LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+// see the LiquidCrystal documentation for a more detailed explanation
+LiquidCrystal lcd( 5, 6, 1, 2, 3, 4 );
+
+//pinched from uberfridge (www.elcojacobs.com)
+float eepromReadFloat(int address){
+   union u_tag {
+     byte b[4];
+     float fval;
+   } u;   
+   u.b[0] = EEPROM.read(address);
+   u.b[1] = EEPROM.read(address+1);
+   u.b[2] = EEPROM.read(address+2);
+   u.b[3] = EEPROM.read(address+3);
+   return u.fval;
+}
+ 
+void eepromWriteFloat(int address, float value){
+   union u_tag {
+     byte b[4];
+     float fval;
+   } u;
+   u.fval=value;
+ 
+   EEPROM.write(address  , u.b[0]);
+   EEPROM.write(address+1, u.b[1]);
+   EEPROM.write(address+2, u.b[2]);
+   EEPROM.write(address+3, u.b[3]);
+}
+
+void saveSettings(void){
+  if(targetTemp != eepromReadFloat(0)){
+     eepromWriteFloat(0, targetTemp);
+  }
+  if(tempDiff != eepromReadFloat(4)){
+     eepromWriteFloat(4, tempDiff);
+  }
+}
+ 
+void loadSettings(void){
+  //used to store EEPROM values for checking before loading
+  float readTargetTemp;
+  float readTempDiff;
+  
+  readTargetTemp = eepromReadFloat(0);
+  if(readTargetTemp > 0 && readTargetTemp < 100) {
+     targetTemp = readTargetTemp;
+  }  
+  
+  readTempDiff = eepromReadFloat(4);
+  if(readTempDiff > 0 && readTempDiff <= 10) {
+     tempDiff = readTempDiff;
+  }  
+  
+}
 
 void findTempSensors()
 {
@@ -105,19 +162,19 @@ void findTempSensors()
        Serial.println("Found DS18B20!");
        sensorCount++;
        if (sensorCount > 1) {
-         memcpy(externaltempAddr,tempAddr,8);
-         Serial.print("Found external sensor: ");
-         for (addrCount = 0; addrCount < 8; addrCount++) {
-           Serial.print(externaltempAddr[addrCount], HEX);
-         }
-         Serial.println();
-         Serial.println("We've found both sensors.");
-         sensorFound = true;
-       } else if (sensorCount > 0) {
          memcpy(internaltempAddr,tempAddr,8);
          Serial.print("Found internal sensor: ");
          for (addrCount = 0; addrCount < 8; addrCount++) {
            Serial.print(internaltempAddr[addrCount], HEX);
+         }
+         Serial.println();
+         Serial.println("We've found both sensors.");
+         sensorsFound = true;
+       } else if (sensorCount > 0) {
+         memcpy(externaltempAddr,tempAddr,8);
+         Serial.print("Found external sensor: ");
+         for (addrCount = 0; addrCount < 8; addrCount++) {
+           Serial.print(externaltempAddr[addrCount], HEX);
          }
          Serial.println();
        }
@@ -134,10 +191,10 @@ void getCurrentTemps()
   int tempData[12];
   int16_t tempRaw;
   
-  if (sensorFound && (sensorConverting < 40) && (sensorConverting > 0)) {
+  if (sensorsFound && (sensorConverting < 10) && (sensorConverting > 0)) {
     //Serial.println("Incrementing sensor wait counter.");
     sensorConverting++;
-  } else if (sensorFound && (sensorConverting == 0)) {
+  } else if (sensorsFound && (sensorConverting == 0)) {
       Serial.println("Sending a conversion request to sensors.");
       //kick off a temp conversion
       onewire.reset();
@@ -148,7 +205,7 @@ void getCurrentTemps()
       onewire.write(0x44);
       //reset quasi-timer
       sensorConverting = 1;
-  } else if (sensorFound && (sensorConverting > 9)) {
+  } else if (sensorsFound && (sensorConverting > 9)) {
       //lets read internal then external sensors
       Serial.println("Reading sensors.");
       //we've hit one second (10 100 millisecond delays)
@@ -209,19 +266,22 @@ void getCurrentTemps()
 
 void setup() {
   
+  //read settings
+  loadSettings();
+  
   //set up serial for debug
   Serial.begin(9600);
   
   //set up TCP server
-  Ethernet.begin(mac, ip, gateway, subnet);
-  server.begin();
+  //Ethernet.begin(mac, ip, gateway, subnet);
+  //server.begin();
 
   // set up the LCD's number of columns and rows:
   Serial.println("Setting up LCD"); 
   lcd.begin(16, 2);
-  lcd.setCursor(2,0);
+  lcd.setCursor(3,0);
   lcd.print("I:");
-  lcd.setCursor(9,0);
+  lcd.setCursor(10,0);
   lcd.print("E:");
   lcd.setCursor(0,1);
   lcd.print("[I]");
@@ -236,12 +296,6 @@ void setup() {
   digitalWrite(heatPin, LOW);
   pinMode(coolPin, OUTPUT);
   digitalWrite(coolPin, LOW);
-  
-  //reset readings array
-   for (int thisReading = 0; thisReading < maxReadings; thisReading++) {
-    internaltempReadings[thisReading] = 0;  
-    externaltempReadings[thisReading] = 0;  
-   }
     
    //search for onewire temp sensor
    delay(5000);
@@ -249,54 +303,100 @@ void setup() {
 }
 
 void switchRelays() {
-  //if cooling and temp less than minimum, switch to idle, turn off relays
-  if (mode == MODE_COOL && currentinternalTemp < (targetTemp - tempDiff)) {
-    Serial.println("Idling because we've been cooling and we've reached target temperature.");
+  Serial.print ("lastCool: ");
+  Serial.print (lastCool);
+  Serial.println();
+  Serial.print ("millis(): ");
+  Serial.print (millis());
+  Serial.println();
+  Serial.print ("timeCooling: ");
+  Serial.print (timeCooling);
+  Serial.println();
+  Serial.print ("Time until cooling is OK: ");
+  Serial.print (millis() - lastCool);
+  Serial.println();
+  if (!sensorsFound) {
+    //no sensors, no relay action. 
     digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, LOW);
+    digitalWrite(coolPin, LOW);
     mode = MODE_IDLE;
+  } else if (mode == MODE_COOL) {
+    //this is where we need to account for overshoot!
+    //if the temperature is still higher than our target temp + estimated idle cooling (overshoot), 
+    //or if the compressor has been running for less than 10 seconds, keep cooling
+    if (currentinternalTemp - (currentinternalTemp * COLD_OVERSHOOT) >= (targetTemp) || (millis() - timeCooling < COMPRESSOR_MIN_RUN)) {
+      Serial.println("Keep cooling, we haven't reached our goal.");
+      Serial.print ("Overshoot temp: ");
+      Serial.print (currentinternalTemp - currentinternalTemp * COLD_OVERSHOOT);
+      Serial.println();
+      digitalWrite(coolPin, HIGH);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_COOL;
+    } else {
+      Serial.println("Idling because we've been cooling and we've reached target temperature.");
+      Serial.print ("Overshoot temp: ");
+      Serial.print (currentinternalTemp - currentinternalTemp * COLD_OVERSHOOT);
+      Serial.println();
+       digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_IDLE;
+      //set last cool counter
+      lastCool = millis();
+    }
   //if heating and temp higher than maximum, switch to idle, turn off relays
-  } else if (mode == MODE_HEAT && currentinternalTemp > (targetTemp + tempDiff)) {
-    Serial.println("Idling because we've been heating and we've reached target temperature.");
-    digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, LOW);
-    mode = MODE_IDLE;
+  } else if (mode == MODE_HEAT) {
+    if (currentinternalTemp <= (targetTemp + tempDiff)) {
+      Serial.println("Keep heating, we haven't reached our goal.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, HIGH);
+      mode = MODE_HEAT;
+    } else {
+      Serial.println("Idling because we've been heating and we've reached target temperature.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_IDLE;
+      //set the last heat counter
+      lastHeat = millis();
+    }
   //if idle and temp inside is higher than outside, and temperature has not reached minimum, keep idling
-  } else if (mode == MODE_IDLE && currentinternalTemp > (targetTemp - tempDiff) && currentexternalTemp < currentinternalTemp) {
-    Serial.println("Keep idling because it's cooler outside and we're above minimum temp.");
-    digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, LOW);
-    mode = MODE_IDLE;
-  //if idle and temp inside is lower than outside, and temperature has not reached maximum, keep idling
-  } else if (mode == MODE_IDLE && currentinternalTemp < (targetTemp + tempDiff) && currentexternalTemp > currentinternalTemp) {
-    Serial.println("Keep idling because it's hotter outside and we're below maximum temp.");
-    digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, LOW);
-    mode = MODE_IDLE;
-  //if idle and temp inside is lower than temp outside, and temperature has reached maximum, time to cool
-  } else if (mode == MODE_IDLE && currentinternalTemp > (targetTemp + tempDiff) && currentexternalTemp > (targetTemp + tempDiff)) {
-    Serial.println("Start cooling because it's hotter outside and we're above maximum temp.");
-    digitalWrite(coolPin, HIGH);
-    digitalWrite(heatPin, LOW);
-    mode = MODE_COOL;
-  //if idle and temp inside is higher than temp outsid, and temperature has reached minimum, time to heat
-  } else if (mode == MODE_IDLE && currentinternalTemp < (targetTemp - tempDiff) && currentexternalTemp < (targetTemp - tempDiff)) {
-    Serial.println("Start heating because it's colder outside and we're below minimum temp.");
-    digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, HIGH);
-    mode = MODE_HEAT;
-  } else if (mode == MODE_COOL && currentinternalTemp >= (targetTemp - tempDiff)) {
-    Serial.println("Keep cooling, we haven't reached our goal.");
-    digitalWrite(coolPin, HIGH);
-    digitalWrite(heatPin, LOW);
-    mode = MODE_COOL;
-  } else if (mode == MODE_HEAT && currentinternalTemp <= (targetTemp + tempDiff)) {
-    Serial.println("Keep heating, we haven't reached our goal.");
-    digitalWrite(coolPin, LOW);
-    digitalWrite(heatPin, HIGH);
-    mode = MODE_HEAT;
+  } else if (mode == MODE_IDLE) {
+    if (currentinternalTemp >= (targetTemp - tempDiff) && currentexternalTemp <= currentinternalTemp) {
+      Serial.println("Keep idling because it's cooler outside and we're above minimum temp.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_IDLE;
+    //if idle and temp inside is lower than outside, and temperature has not reached maximum, keep idling
+    } else if (currentinternalTemp <= (targetTemp + tempDiff) && currentexternalTemp >= currentinternalTemp) {
+      Serial.println("Keep idling because it's hotter outside and we're below maximum temp.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_IDLE;
+     //if idle and temp inside is lower than temp outside, but we are within startup delay period, or resting the compressor, idle
+    } else if (currentinternalTemp >= (targetTemp + tempDiff) && currentexternalTemp >= currentinternalTemp && ((lastCool > 0 && (millis() - lastCool) < REST_COMPRESSOR) || millis() < STARTUP_DELAY)) {
+      Serial.println("In cooling range, but compressor has been on too recently, or startup delay not met.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_IDLE;    
+      //if idle and temp inside is lower than temp outside, and temperature has reached maximum, time to cool
+    } else if (currentinternalTemp >= (targetTemp + tempDiff) && currentexternalTemp >= currentinternalTemp) {
+      Serial.println("Start cooling because it's hotter outside and we're above maximum temp, and we have reached minimum wait time.");
+      digitalWrite(coolPin, HIGH);
+      digitalWrite(heatPin, LOW);
+      mode = MODE_COOL;
+      //set last idle counter
+      lastIdle = millis();
+      timeCooling = millis();
+      //if idle and temp inside is higher than temp outsid, and temperature has reached minimum, time to heat
+    } else if (mode == MODE_IDLE && currentinternalTemp <= (targetTemp - tempDiff) && currentexternalTemp <= currentinternalTemp) {
+      Serial.println("Start heating because it's colder outside and we're below minimum temp.");
+      digitalWrite(coolPin, LOW);
+      digitalWrite(heatPin, HIGH);
+      mode = MODE_HEAT;
+      //set last idle counter
+      lastIdle = millis();
+    }
   } else {
-    //default position is to idle
+    //default position is to idle, catch all in case of a logic error
     Serial.println("No matched condition, keep idling.");
     digitalWrite(coolPin, LOW);
     digitalWrite(heatPin, LOW);
@@ -306,9 +406,9 @@ void switchRelays() {
 
 void updateLCD() {
     //show current temp
-    lcd.setCursor(4,0);
+    lcd.setCursor(5,0);
     lcd.print(currentinternalTemp, 1);
-    lcd.setCursor(11,0);
+    lcd.setCursor(12,0);
     lcd.print(currentexternalTemp, 1);
     //Serial.write(currentTemp);
     //show target temp
@@ -335,7 +435,7 @@ void updateLCD() {
     }
 }
 
-void checkClients() {
+/* void checkClients() {
     //handle TCP client
     EthernetClient client = server.available();
     if (client) {
@@ -394,14 +494,14 @@ void checkClients() {
     // close the connection:
     client.stop();
     }
-}
+} */
 
 void loop() {
 
     //get temperature reading for primary temp
     //Serial.println("Getting temperature");
     getCurrentTemps();
-
+    
     //handle keypad
     int button = getButton();
     //Serial.println("Geting buttons");
@@ -414,7 +514,7 @@ void loop() {
     updateLCD();
     
     //handle tcp ip client
-    checkClients();
+    //checkClients();
    
    //small delay
    delay(100); 
@@ -463,6 +563,7 @@ void handleButton(int button) {
         case BUTTON_SELECT :
           break;
      }
+    saveSettings();
   } 
 }
 
